64e8952eb0a82c56625c92a1a14671e6b09471fc
Unknown <gaobingbing3073707@163.com>
2019-01-07 16:49:16 +0800

    測試圖片插件

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
 
 class UploadController extends Controller
 {
+    public $imageManager;
+    public function __construct(ImageManager $imageManager)
+    {
+        $this->imageManager = $imageManager;
+    }
+
     public function image(Request $request){
-        $filename = $request->file('file')->store('/article');
+
+
+        $file = $request->file('file');
+
+        $fileExtension = $file->getClientOriginalExtension();
+
+        $filename = str_random();
+
+        return $this->imageManager->make($file)->resize(400,300)->save("/uploads/images/$filename.$fileExtension");
+
         return response()->json(['error'=>false,'path'=>config('filesystems.disks.public.url').'/'.$filename]);
     }
+
 }
