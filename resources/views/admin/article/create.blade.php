be13b661ef34c0464dfbd10c48695e98e6a81fc6
Unknown <gaobingbing3073707@163.com>
2019-01-07 14:40:01 +0800

    代码插件

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -69,14 +69,14 @@
                 height: 300,
                 language:"zh_CN",
                 plugins: [
-                    'link imageupload',
+                    'link imageupload codesample',
                     'advlist autolink lists link image charmap print preview hr anchor pagebreak',
                     'searchreplace wordcount visualblocks visualchars code fullscreen',
                     'insertdatetime media nonbreaking save table contextmenu directionality',
                     'emoticons template paste textcolor colorpicker textpattern imagetools'
                 ],
                 convert_urls: false,
-                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | imageupload',
+                toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | imageupload codesample',
                 toolbar2: 'print preview media | forecolor backcolor emoticons',
                 image_advtab: true,
                 imageupload_url: '/admin/upload'
