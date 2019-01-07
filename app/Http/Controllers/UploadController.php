48d3643b0dcfaf28d859b47fcbe7e62cd4ac4aec
Unknown <gaobingbing3073707@163.com>
2019-01-07 17:32:53 +0800

    图片打水印

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,28 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Intervention\Image\ImageManager;
+use Illuminate\Support\Facades\Log;
+use Intervention\Image\Facades\Image;
 
 class UploadController extends Controller
 {
-    public $imageManager;
-    public function __construct(ImageManager $imageManager)
-    {
-        $this->imageManager = $imageManager;
-    }
+    protected $allowed_ext = ["png", "jpg", "gif", 'jpeg'];
 
     public function image(Request $request){
-
-
         $file = $request->file('file');
+        $filename = $this->save($file,'article',auth()->id(),800);
+        if ($filename){
+            return response()->json(['error'=>false,'path'=>$filename['path']]);
+        }else{
+            return response()->json(['error'=>true]);
+        }
+    }
 
-        $fileExtension = $file->getClientOriginalExtension();
+    /**
+     * @param $file  [文件]
+     * @param $folder [目錄]
+     * @param $file_prefix [前綴]
+     * @param bool $max_width [寬度]
+     * @return array|bool
+     */
+    public function save($file, $folder, $file_prefix, $max_width = false)
+    {
+        // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
+        // 文件夹切割能让查找效率更高。
+        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
-        $filename = str_random();
+        // 文件具体存储的物理路径，`public_path()` 获取的是 `public` 文件夹的物理路径。
+        // 值如：/home/vagrant/Code/larabbs/public/uploads/images/avatars/201709/21/
+        $upload_path = public_path() . '/' . $folder_name;
 
-        return $this->imageManager->make($file)->resize(400,300)->save("/uploads/images/$filename.$fileExtension");
+        // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
-        return response()->json(['error'=>false,'path'=>config('filesystems.disks.public.url').'/'.$filename]);
+        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
+        // 值如：1_1493521050_7BVc9v9ujP.png
+        $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
+
+        // 如果上传的不是图片将终止操作
+        if ( ! in_array($extension, $this->allowed_ext)) {
+            return false;
+        }
+
+        // 将图片移动到我们的目标存储路径中
+        $file->move($upload_path, $filename);
+
+        // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
+        return [
+            'path' => config('app.url') . "/$folder_name/$filename"
+        ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        Log::info($file_path);
+        // 先实例化，传参是文件的磁盘物理路径
+        $image = Image::make($file_path);
+
+        // 进行大小调整的操作
+        $image->resize($max_width, null, function ($constraint) {
+
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        $insert_path = public_path() . '/img/logo.png';
+
+        //给图片打水印
+        $image->insert($insert_path,'bottom-right', 1, 1);
+        // 对图片修改后进行保存
+        $image->save();
     }
 
 }
